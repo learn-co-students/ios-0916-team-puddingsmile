e60e5847c9d9fd77472fb039ced78a8ee19ea6a8
Alex Mason <apmason119@gmail.com>
2016-11-21 14:29:23 -0500

    mapView functions

--- GroupProject/MapView.swift
+++ GroupProject/MapView.swift
@@ -11,12 +11,9 @@ import MapKit
 
 class MapView: UIView {
     
+    let locationArray = DataStore.sharedInstance.markets
     let mapView = MKMapView()
-    
-    
-    
-    
-    
+    var mapItemList: [MKMapItem] = []
     
     
     
@@ -26,4 +23,40 @@ class MapView: UIView {
     }
     
     
+    func addAnnotationsToMap() {
+        var annotations: [MKAnnotation] = []
+        for location in mapItemList {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.placemark.coordinate
+            
+            annotations.append(annotation)
+        }
+        let region = MKCoordinateRegion(center: annotations[0].coordinate, span: MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04))
+        mapView.setRegion(region, animated: true)
+        mapView.addAnnotation(annotations as! MKAnnotation)
+    }
+    
+    
+    func convertMarketsToMapItem() {
+        print("convert item called")
+        for location in locationArray {
+            let longitude = Double(location.longitude!)
+            let latitude = Double(location.latitude!)
+            let placemark = MKPlacemark(coordinate: CLLocationCoordinate2DMake(latitude!, longitude!))
+            mapItemList.append(MKMapItem(placemark: placemark))
+        }
+    }
+    
+    //func mapView(
+    
+    
+    
+    
+    
 }
+
+
+
+
+
+
