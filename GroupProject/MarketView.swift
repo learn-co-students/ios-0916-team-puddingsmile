e418de95f79923aae78675742db59b9cf2c2fc79
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2016-11-29 10:12:55 -0500

    reformmatted the table view for a more aesthetic look

--- GroupProject/MarketView.swift
+++ GroupProject/MarketView.swift
@@ -53,31 +53,44 @@ class MarketView: UIView {
         Bundle.main.loadNibNamed("MarketViewPrototype", owner: self, options: nil)
         
         addSubview(contentView)
+        
+        containerView = UIView()
+        contentView.addSubview(containerView)
+        
+        nameLabel = UILabel()
+        containerView.addSubview(nameLabel)
+        
+        addressLabel = UILabel()
+        containerView.addSubview(addressLabel)
+        
+        timeLabel = UILabel()
+        containerView.addSubview(timeLabel)
+        
+        dayLabel = UILabel()
+        containerView.addSubview(dayLabel)
+    }
+    
+    
+    func setConstraints() {
+        //Content View
         contentView.translatesAutoresizingMaskIntoConstraints = false
         contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        contentView.backgroundColor = UIColor.lightGray
+        contentView.backgroundColor = UIColor.themeTertiary
         
-        nameLabel = UILabel()
-        nameLabel.font = UIFont.systemFont(ofSize: 24)
-        contentView.addSubview(nameLabel)
-      
+        //Container View
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.95).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: contentView.bounds.height * 0.9).isActive = true
+        containerView.layer.cornerRadius = 10
+        containerView.backgroundColor = UIColor.themePrimary
         
-        addressLabel = UILabel()
-        contentView.addSubview(addressLabel)
-        
-        timeLabel = UILabel()
-        contentView.addSubview(timeLabel)
-        
-        dayLabel = UILabel()
-        contentView.addSubview(dayLabel)
-    }
-    
-    
-    func setConstraints() {
         //Name Label
+        nameLabel.font = UIFont.systemFont(ofSize: 24, weight: 8)
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: contentView.bounds.height * 0.2).isActive = true
@@ -98,7 +111,7 @@ class MarketView: UIView {
         timeLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: self.bounds.height * 0.7).isActive = true
         timeLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3).isActive = true
         timeLabel.heightAnchor.constraint(equalToConstant: contentView.bounds.height * 0.15).isActive = true
-        timeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentView.bounds.width * 0.2).isActive = true
+        timeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentView.bounds.width * 0.25).isActive = true
         
         //Day Label
         dayLabel.translatesAutoresizingMaskIntoConstraints = false
