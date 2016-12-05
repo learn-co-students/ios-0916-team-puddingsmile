7a58c05f5a27c383590e8e142e9c8de7349f7951
Ben Su <bensu1013@hotmail.com>
2016-12-05 14:45:36 -0500

    minor changes

--- GroupProject/EditorStore.swift
+++ GroupProject/EditorStore.swift
@@ -19,8 +19,49 @@ class EditorStore {
     var endTimes: String?
     var ebt: String?
     
+    var lat: String?
+    var long: String?
+    
     
     init() {}
+    
+    func packageData() -> [String : String] {
+        var data: [String : String] = [:]
+        if let name = self.name {
+            data["name"] = name
+        }
+        if let address = self.address {
+            data["address"] = address
+        }
+        if let city = self.city {
+            data["city"] = city
+        }
+        if let endSeason = self.endSeason {
+            if let startSeason = self.startSeason {
+                data["openDate"] = startSeason
+                data["closeDate"] = endSeason
+            }
+        }
+        if let days = self.days {
+            data["days"] = days
+        }
+        if let endTimes = self.endTimes {
+            if let startTimes = self.startTimes {
+                data["startTime"] = startTimes
+                data["endTime"] = endTimes
+            }
+        }
+//        if let ebt = self.ebt {
+//            data["ebt"] = ebt
+//        }
+        if let lat = self.lat,
+            let long = self.long {
+            data["latitude"] = lat
+            data["longitude"] = long
+        }
+        
+        return data
+    }
     
     func resetProperties() {
         name = nil
