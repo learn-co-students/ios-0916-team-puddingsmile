7e5512ecca256f9136ffdc4014f85cd8dbccac18
Alex Mason <apmason119@gmail.com>
2016-12-06 16:25:20 -0500

    Xib working

--- GroupProject/ProposedMarketView.swift
+++ GroupProject/ProposedMarketView.swift
@@ -9,22 +9,29 @@
 import UIKit
 
 class ProposedMarketView: UIView {
+    
+    var containerView: UIView!
+    var nameLabel: UILabel!
+    var addressLabel: UILabel!
+    var timeLabel: UILabel!
+    var dayLabel: UILabel!
 
     @IBOutlet var contentView: UIView!
     
-    let label: UILabel = {
-        let label = UILabel()
-        
-        
-        
-        
-        return label
-    }()
-    
+//    let testLabel: UILabel = {
+//        let label = UILabel()
+//        label.text = "Its working"
+//        label.textColor = UIColor.white
+//        label.backgroundColor = UIColor.black
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        return label
+//    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
+        setConstraints()
         print("view created")
 
     }
@@ -32,26 +39,111 @@ class ProposedMarketView: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
+        setConstraints()
         print("decoder init")
     }
     
     func commonInit() {
         
         Bundle.main.loadNibNamed("AddedMarket", owner: self, options: nil)
-        
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        
         self.addSubview(contentView)
+                
+        containerView = UIView()
+        contentView.addSubview(containerView)
         
-        print("Setting up contentView")
-
+        nameLabel = UILabel()
+        containerView.addSubview(nameLabel)
+        
+        addressLabel = UILabel()
+        containerView.addSubview(addressLabel)
+        
+        timeLabel = UILabel()
+        containerView.addSubview(timeLabel)
+        
+        dayLabel = UILabel()
+        containerView.addSubview(dayLabel)
+    }
+    
+    func setConstraints() {
+        //Content View
+        contentView.translatesAutoresizingMaskIntoConstraints = false
         contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.backgroundColor = UIColor.white
+        
+        //Container View
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.95).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: contentView.bounds.height * 0.9).isActive = true
+        containerView.layer.cornerRadius = 10
+        
+        let background: UIImage! = UIImage(named: "xibBackground.png")
+        containerView.backgroundColor = UIColor(patternImage: background)
+        
+        //Name Label
+        nameLabel.font = UIFont.systemFont(ofSize: 24, weight: 8)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: contentView.bounds.height * 0.2).isActive = true
+        nameLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.9).isActive = true
+        nameLabel.heightAnchor.constraint(equalToConstant: contentView.bounds.height * 0.25).isActive = true
+        nameLabel.textAlignment = NSTextAlignment.center
+        
+        //Address Label
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        addressLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        addressLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: self.bounds.height * 0.5).isActive = true
+        addressLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.9).isActive = true
+        addressLabel.heightAnchor.constraint(equalToConstant: contentView.bounds.height * 0.15).isActive = true
+        addressLabel.textAlignment = NSTextAlignment.center
+        
+        //Time Label
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        timeLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: self.bounds.height * 0.7).isActive = true
+        timeLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.3).isActive = true
+        timeLabel.heightAnchor.constraint(equalToConstant: contentView.bounds.height * 0.15).isActive = true
+        timeLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentView.bounds.width * 0.25).isActive = true
+        
+        //Day Label
+        dayLabel.translatesAutoresizingMaskIntoConstraints = false
+        dayLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: self.bounds.height * 0.7).isActive = true
+        dayLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3).isActive = true
+        dayLabel.heightAnchor.constraint(equalToConstant: self.bounds.height * 0.15).isActive = true
+        dayLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: contentView.bounds.width * 0.6).isActive = true
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//extension ProposedMarketView {
+//    
+//    func setupContentViewLabel() {
+//        print("Setup content view called")
+//        contentView.addSubview(testLabel)
+//        
+//        testLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+//        testLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+//        testLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3).isActive = true
+//        testLabel.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.3).isActive = true
+//    }
+//    
+//    
+//}
 
 
 
