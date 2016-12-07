30c2e12b5ed470211b0183274047bd6577982b39
chainsawbucktooth <chainsaw.bucktooth@gmail.com>
2016-12-07 13:20:26 -0500

    reviewed conflicts with master and reconciled the changes that I want to keep

--- GroupProject/MapViewController.swift
+++ GroupProject/MapViewController.swift
@@ -17,14 +17,8 @@ class MapViewController: UIViewController, MapViewDelegate, NavBarViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
         setLayout()
         
-=======
-        let mapView = MapView(frame: self.view.frame)
-        mapView.mapDelegate = self
-        self.view = mapView
->>>>>>> master
     }
     
     func getInfo(market: Market) {
